45e006248a830f44bc554c5604d7ceda0c361a73
Aswer3570 <fedreg2012@yandex.ru>
2024-04-27 14:23:38 +0400

    Fi

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,17 @@ import './index.scss'
 import App from './App.tsx'
 import InitializationProvider from './InitializationProvider'
 
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        id?: string;
+        // Другие свойства, если они есть
+      };
+    };
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<WebAppProvider options={{ smoothButtonsTransition: true	}}>
 		<BrowserRouter>
